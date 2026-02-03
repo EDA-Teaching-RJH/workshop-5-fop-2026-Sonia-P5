e8db745cc56c241111577212d5331d915d502afb
Sonia-P5 <sp102@kent.ac.uk>
2026-02-03 12:51:48 +0000

    save

--- camel.py
+++ camel.py
@@ -1,1 +1,9 @@
 # Enter your code here
+camel=input('Insert text in the style of camel case: ')
+snake=''
+for char in camel:
+    if char.isupper():
+        snake+='_'+char.lower()
+    else:
+        snake+=char
+print('snake_case:', snake)
